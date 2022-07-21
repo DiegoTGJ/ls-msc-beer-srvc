700a332d3a64fde3eef9f99de84fc868814bbc64
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-21 08:44:28 -0400

    Initial project files. Closes #1

---
+++ src/main/java/pdtg/lsmscbeersrvc/LsMscBeerSrvcApplication.java
@@ -0,0 +1,13 @@
+package pdtg.lsmscbeersrvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsMscBeerSrvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LsMscBeerSrvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pdtg/lsmscbeersrvc/LsMscBeerSrvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pdtg.lsmscbeersrvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsMscBeerSrvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
