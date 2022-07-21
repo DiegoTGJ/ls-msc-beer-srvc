22e49797c8f782642ac5b1378cd8819c85304546
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-21 09:08:07 -0400

    added beer controller. Closes #3

---
+++ src/main/java/pdtg/lsmscbeersrvc/web/controller/BeerController.java
@@ -0,0 +1,40 @@
+package pdtg.lsmscbeersrvc.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pdtg.lsmscbeersrvc.web.model.BeerDto;
+
+import java.util.UUID;
+
+/**
+ * Created by Diego T. 21-07-2022
+ */
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping({"/{beerId}"})
+    ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+    }
+
+    @PutMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateBeerById(@PathVariable UUID beerId,@RequestBody BeerDto beerDto){
+        //todo impl
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeerById(@PathVariable UUID beerId){
+        //todo impl
+    }
+}
