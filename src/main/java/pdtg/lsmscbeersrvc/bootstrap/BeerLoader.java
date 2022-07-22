7e10e8bfd1d710f1deeda1dc3b0bed74a8b423a9
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-22 01:15:27 -0400

    Added beer loading. Closes #7

---
+++ src/main/java/pdtg/lsmscbeersrvc/bootstrap/BeerLoader.java
@@ -0,0 +1,48 @@
+package pdtg.lsmscbeersrvc.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pdtg.lsmscbeersrvc.domain.Beer;
+import pdtg.lsmscbeersrvc.repositories.BeerRepository;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Diego T. 22-07-2022
+ */
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+
+    @Override
+    public void run(String... args) {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects(){
+        if(beerRepository.count()==0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .upc(16546546546654L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .upc(545454545L)
+                    .price(new BigDecimal("13.95"))
+                    .build());
+        }
+        System.out.println("Loaded Beers: "+beerRepository.count());
+    }
+}
