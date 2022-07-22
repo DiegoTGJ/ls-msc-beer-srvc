060906d496d2d9c31902a7c097843a09d4322791
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-22 00:47:58 -0400

    Created JPA entity for Beer. Closes #5

---
+++ src/main/java/pdtg/lsmscbeersrvc/domain/Beer.java
@@ -0,0 +1,53 @@
+package pdtg.lsmscbeersrvc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import pdtg.lsmscbeersrvc.web.model.BeerStyleEnum;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by Diego T. 22-07-2022
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar",updatable = false,nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
